9133c56c15ccd2c06d20ec93dc05ef5557ebd711
Aravind Raj <160577717+arvindr333@users.noreply.github.com>
2026-06-23 15:29:09 +0530

    Update urls.py

--- CandidLedger/candidledger_project/candidledger_project/urls.py
+++ CandidLedger/candidledger_project/candidledger_project/urls.py
@@ -1,14 +1,26 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
+
+from django.contrib.sitemaps.views import sitemap
+from candidledger_app.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('candidledger_app.urls')),
+
+    path(
+        'sitemap.xml',
+        sitemap,
+        {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'
+    ),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
